084b01ec04c4a597c835dbcfb3d9de6d1a7cb1ed
Skaradams <skaradams69@gmail.com>
2012-06-24 12:23:11 +0200

    let's rock

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Takethis::Application.config.secret_token = '3eda9d3fa1a699a0d3f059ffe0b5f1687d90d72657c61ab6e719ac1239a8faab979294a95b5cc9aafd31e025e5e3f9c7b005d7cb97ef0b5ce43c3480c9abf69c'
